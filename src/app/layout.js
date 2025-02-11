fd25ac2e9e7dadd21c4fe327b98161063ed741ce
Jay Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2025-02-11 12:19:23 +0000

    desc updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,7 +12,7 @@ const geistSans = localFont({
 export const metadata = {
   title: "Jay Tillu",
   description:
-    "I'm Jay Tillu, a passionate Cybersecurity Engineer with expertise in ISO 27001, Penetration Testing, and Threat Management. I've completed Google Cybersecurity & Azure Certifications. Let's connect!",
+    "Hello! I'm Jay Tillu, an Information Security Engineer at Simple2Call. I have expertise in security frameworks and compliance, including NIST, ISO 27001, and ISO 27701. My specialities include Vulnerability Management, Threat Analysis, and Incident Response. I have also earned certifications in Google Cybersecurity and Microsoft Azure. I’m always eager to connect and discuss cybersecurity—let's get in touch!",
 };
 
 export default function RootLayout({ children }) {
